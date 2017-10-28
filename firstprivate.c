3cb1f585d7ceb69fd50a245c87fcdd757e7bdd60
CURIOUSER005 <shailesh.kolhapur@gmail.com>
2017-10-28 23:23:19 +0530

    Update firstprivate.c
    Added private() to omp region to show differences

--- firstprivate.c
+++ firstprivate.c
@@ -2,15 +2,16 @@
     #include<stdio.h>
     #include<stdlib.h>
     #include<omp.h>
-     int main()
+     void main()
      {
          int i,a=10,b=5;
-         #pragma omp parallel for firstprivate(a) num_threads(1)
+         #pragma omp parallel for firstprivate(a) private(b) num_threads(1)
          for(i=1;i<5;i++)
          {
+             b++;  //In Linux it will assign value b=0 and then increament operator will work. This is functionality of private()
              a = a+b;
              printf("A is after %d iteration : %d\n",i,a);
          }
-         printf("A outside firstprivate is %d\n",a);
-         return 0;
+         printf("A outside omp region is %d\n",a);
+         
      }
